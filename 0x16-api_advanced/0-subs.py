00a3a6e6e35ccac33826cc5c3152c3eaa12f813d
Secatricia <3560@holbertonschool.com>
2022-05-31 21:27:26 +0000

    Requirement

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,13 +2,14 @@
 """queries the Reddit API and returns the number of subscribers"""
 import requests
 
+
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json"
     queries = requests.get(url.format(subreddit),
                            headers={"Querie": "number of subscribers"},
                            allow_redirects=False)
 
-    if queries.status_code == 200: #if the request succeeded
+    if queries.status_code == 200:  # if the request succeeded
         return queries.json()["data"]["subscribers"]
 
-    return 0  #If not a valid subreddit
+    return 0  # If not a valid subreddit
