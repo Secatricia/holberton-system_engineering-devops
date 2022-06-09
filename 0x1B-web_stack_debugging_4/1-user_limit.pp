fe0a19176ca53d187cb1120053462a1cad2af942
Secatricia <3560@holbertonschool.com>
2022-06-09 21:18:38 +0000

    Add task 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+# login with the holberton user and open a file 
+# without any error message
+exec { 'Correct hard':
+  command  => 'sudo sed -i \'s/nofile 5/nofile 30000/\' /etc/security/limits.conf',
+  provider => shell,
+}
+exec { 'Correct soft':
+  command  => 'sudo sed -i \'s/nofile 4/nofile 10000/\' /etc/security/limits.conf',
+  provider => shell,
+}
